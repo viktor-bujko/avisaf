101b671a64ff731d5f2b7d1bd8e2835f627d6336
Viktor Bujko <viktorbjk@gmail.com>
2021-04-16 17:07:08 +0200

    Merged common implementation in super class

--- avisaf/classification/vectorizers.py
+++ avisaf/classification/vectorizers.py
@@ -186,39 +186,40 @@ class Doc2VecAsrsReportVectorizer(AsrsReportVectorizer):
 
 class Word2VecAsrsReportVectorizer(AsrsReportVectorizer):
 
-    def __init__(self):
-        logging.debug("Loading spacy model")
+    def __init__(self, vectors=None):
         self._nlp = spacy.load('en_core_web_md')
+        self._vectors = vectors
 
-    def build_feature_vectors(self, texts, target_labels_shape: int, train: bool = False) -> np.ndarray:
-        """
+    @staticmethod
+    def preprocess(texts):
 
-        :param texts:
-        :type texts: list of str
-        :param target_labels_shape:
-        :param train:
-        :return:
-        """
-
-        spacy_doc_vectors = []
         preprocessed = []
         for text in texts:
             text = text.lower()
             text = re.sub(r'([0-9]{1,2});([0-9]{1,3})', r'\1,\2', text)
+            text = re.sub(r'fl[0-9]{2,3}', 'flight level', text)
+            text = re.sub(r'rwy', r'runway', text)
+            text = re.sub(r'(z){3,}[0-9]*', r'airport', text)
             preprocessed.append(text)
 
-        texts = preprocessed
+        return preprocessed
 
-        for doc_vector_batch in self._generate_vectors(texts, 256):
-            spacy_doc_vectors.append(doc_vector_batch)
+    def build_feature_vectors(self, texts: list, target_labels_shape: int, train: bool = False) -> np.ndarray:
+        logging.debug('Started vectorization')
+
+        texts = self.preprocess(texts)
+
+        doc_vectors = []
+        for doc_vector_batch in self._generate_vectors(texts, self._vectors, 256):
+            doc_vectors.append(doc_vector_batch)
             print("===========================")
 
-        result = np.array(np.concatenate(spacy_doc_vectors, axis=0))
+        result = np.array(np.concatenate(doc_vectors, axis=0))
         logging.debug(f'Vectorized {result.shape[0]} texts')
 
         return result
 
-    def _generate_vectors(self, texts, batch: int = 50):
+    def _generate_vectors(self, texts, vectors, batch: int = 50):
         oov = set()
         doc_vectors = []
         for doc in self._nlp.pipe(texts, disable=['ner'], batch_size=batch):
@@ -227,104 +228,71 @@ class Word2VecAsrsReportVectorizer(AsrsReportVectorizer):
                 # if token.conjuncts:
                 # print(f'"{token.text}" conjuncts: {[conj.text for conj in token.conjuncts]}')
                 if token.is_punct:
-                    # ignore the punctuation
+                    # ignoring the punctuation
                     continue
-                if token.is_oov:
-                    oov.add(token.text)
-                    logging.warning(f'Word {token.text} is not in vocabulary! Skipping')
-                    continue
-                if token.pos_ == 'PRON':
-                    # token is pronoun which would be replaced by "-PRON-" tag otherwise
+                if token.pos_ == 'PRON' and token.text in vectors:
+                    # taking "we", "he" etc instead of "-PRON-"
                     lemmas.append(token.text)
+                    continue
+                if token.is_stop:
+                    # ignoring the stop words, but **after** taking the pronouns
                     continue
                 if token.like_num:
                     # replacing all numbers by common tag
                     lemmas.append("number")
+                    continue
+                if token.lemma_ not in vectors:
+                    # ignoring word which don't have vector representation
+                    logging.warning(f'Word "{token.text}" with lemma "{token.lemma_} not in vocabulary"')
+                    oov.add(token.text)
+                    continue
 
                 lemmas.append(token.lemma_)
-            doc_vector = Doc(self._nlp.vocab, words=lemmas).vector * (len(lemmas) / 500)
+
+            doc_vector = self._get_doc_vector(lemmas)  #
             doc_vectors.append(doc_vector)
 
         yield np.array(doc_vectors)
 
-        # with open('out_of_vocab.txt', 'w') as oov_file:
-        #    print(*sorted(list(oov)), sep='\n', file=oov_file)
+        with open('out_of_vocab_20210416.txt', 'w') as oov_file:
+            logging.debug("Saving unused words")
+            print(*sorted(list(oov)), sep='\n', file=oov_file)
+
+    def _get_doc_vector(self, lemmas):
+        pass
 
 
-class GoogleNewsWord2VecAsrsReportVectorizer(AsrsReportVectorizer):
+class SpaCyWord2VecAsrsReportVectorizer(Word2VecAsrsReportVectorizer):
     def __init__(self):
-        model_path = Path('..', '..', 'gensim-data', 'GoogleNews-vectors-negative300.bin')
+        logging.debug("Loading spacy model")
+        self._nlp = spacy.load('en_core_web_md')
+        super().__init__(self._nlp.vocab)
+
+    def _get_doc_vector(self, lemmas):
+        return Doc(self._nlp.vocab, words=lemmas).vector * (len(lemmas) / 500)
+
+
+class GoogleNewsWord2VecAsrsReportVectorizer(Word2VecAsrsReportVectorizer):
+    def __init__(self):
+        logging.debug(Path().absolute())
+        model_path = Path('gensim-data', 'GoogleNews-vectors-negative300.bin')
         if not model_path.exists():
             logging.warning("Pre-trained GoogleNews model used for vectorization has not been found.")
-            if input("Do you want to download and unzip the model (1.5 Gb zipped size)? (y/N)").lower() == 'y':
+            if input("Do you want to download and unzip the model (1.5 Gb zipped size)? (y/N) ").lower() == 'y':
                 logging.debug('(down)LOADING')
                 model_path = dnld.load('word2vec-google-news-300', return_path=True)
                 print(f'MODEL PATH: {model_path}')
+            else:
+                sys.exit(1)
 
         # TODO: Download and unzip the model
 
         print("Loading large GoogleNews model. May take a while.")
         self._model = KeyedVectors.load_word2vec_format(model_path, binary=True)
-        self._nlp = spacy.load('en_core_web_md')
+        super().__init__(self._model.wv)
 
-    def build_feature_vectors(self, texts: list, target_labels_shape: int, train: bool = False) -> np.ndarray:
-
-        logging.debug('Started vectorization')
-
-        # texts = [str(text).lower() for text in texts]
-        preprocessed = []
-
-        for text in texts:
-             text = text.lower()
-             text = re.sub(r'([0-9]{1,2});([0-9]{1,3})', r'\1,\2', text)
-             text = re.sub(r'fl[0-9]{2,3}', 'flight level', text)
-             text = re.sub(r'rwy', r'runway', text)
-             text = re.sub(r'(z){3,}[0-9]*', r'airport', text)
-             preprocessed.append(text)
-
-        texts = preprocessed
-        doc_vectors = []
-
-        for doc_vector_batch in self._generate_vectors(texts):
-            doc_vectors.append(doc_vector_batch)
-
-        result = np.array(np.concatenate(doc_vectors, axis=0))
-        logging.debug(f'Vectorized {result.shape[0]} texts')
-
-        return result
-
-    def _generate_vectors(self, texts, batch: int = 50):
-
-        doc_vectors = []
-        for doc in self._nlp.pipe(texts, disable=['ner'], batch_size=batch):
-            lemmas = []
-            for token in doc:
-                if token.is_punct:
-                    continue
-                if token.pos_ == 'PRON' and token.text in self._model.wv:
-                    lemmas.append(token.text)
-                    continue
-                if token.like_num:
-                    lemmas.append('number')
-                    continue
-                if token.lemma_ in self._model.wv:
-                    lemmas.append(token.lemma_)
-                    continue
-                if token.text in self._model.wv:
-                    lemmas.append(token.text)
-                    continue
-                else:
-                    logging.warning(f'Word "{token.text}" does not have its lemma "{token.lemma_}" nor the text in the vocabulary. Skipping!')
-            # computes the average of vectors
-            # doesnt take into account the "weight" of each word -> "pilot" in 5 words sentence has 1/5 weight whereas
-            # "pilot" in 200 words sentence has 1/200 weight
-            # checking the avg number of words in the reports
-
-            # word2vec space is invariant to constant multiplication
-            doc_vector = np.mean(self._model[lemmas] * (len(lemmas) / 500), axis=0)
-            doc_vectors.append(doc_vector)
-
-        yield np.array(doc_vectors)
+    def _get_doc_vector(self, lemmas):
+        return np.mean(self._model[lemmas] * (len(lemmas) / 500), axis=0)
 
 
 if __name__ == '__main__':
